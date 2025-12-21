fac8f58c4c34a219f800a12213e45a790a78021e
okofka <nazar.dmytroo@gmail.com>
2025-12-21 20:52:08 +0200

    little changes

--- ITBus/obj/Debug/net8.0/ITBus.AssemblyInfo.cs
+++ ITBus/obj/Debug/net8.0/ITBus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITBus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0780cdb26f9579c6c00039a822c3d2a8dcdb09b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ce18d6ec156cab097e05346389d796802205af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITBus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITBus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
